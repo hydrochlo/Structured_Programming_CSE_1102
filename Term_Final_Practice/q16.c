701b23e9d6ad03b2739e6199067a1a2ffeb6e13e
Shahadat Hossain <sht.kcn@gmail.com>
2026-07-10 15:56:35 +0600

    16 no. solved

--- Term_Final_Practice/q16.c
+++ Term_Final_Practice/q16.c
@@ -5,14 +5,15 @@ QB: 13 Page (1(a, b))
 
 int main(){
 
-
-
-
-
-
-
-
-
-
+    int i, j;
+    for(i=0; i<=10; i++){
+        i = (i+1)%5;
+        for(j=1; j<=i; j++) {printf(" %d ", (i+j));}
+        printf("\n");
+    }
     return 0;
 }
+/*
+Output:
+
+*/
