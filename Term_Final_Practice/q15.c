446bb9b2bd8d7f79913ffd1a964327d2ba74f393
Shahadat Hossain <sht.kcn@gmail.com>
2026-07-10 14:26:39 +0600

    15no. solved

--- Term_Final_Practice/q15.c
+++ Term_Final_Practice/q15.c
@@ -7,18 +7,18 @@ int main(){
     int arr[10][6] = {
         // CSE Student Data (Discipline Code: 2)
     // dis  id    att ct secA secB
-        {2, 2001, 9, 25, 24, 22},
-        {2, 2002, 8, 18, 15, 20},
-        {2, 2003, 10, 28, 29, 27},
-        {2, 2004, 7, 12, 14, 18},
-        {2, 2005, 9, 22, 35, 12}, // Inconsistent data row
+        {2, 2001, 9, 25, 10, 12},
+        {2, 2002, 8, 28, 4, 20},
+        {2, 2003, 10, 28, 19, 5},
+        {2, 2004, 7, 12, 6, 18},
+        {2, 2005, 9, 22, 15, 8}, // Inconsistent data row
         
         // ECE Student Data (Discipline Code: 9)
         {9, 9001, 18, 20, 25, 22},
         {9, 9002, 9, 24, 26, 25},
         {9, 9003, 12, 15, 12, 15}, // Inconsistent data row
-        {9, 9004, 7, 22, 20, 21},
-        {9, 9005, 10, 29, 28, 30}
+        {9, 9004, 7, 22, 20, 1},
+        {9, 9005, 10, 29, 20, 3}
     };
 
     int cse_sec_A = 0, cse_std = 0, ece_sec_A = 0, ece_std = 0, cnt_wrong_data = 0;
@@ -50,6 +50,15 @@ int main(){
     }
 
     printf("Wrong data inputed = %d", cnt_wrong_data);
+
+    int interesting_marks = 0;
+    for(int i=0; i<10; i++){
+        if(arr[i][3]>=25 && (arr[i][4]+arr[i][5]) < 25){
+            interesting_marks++;
+        }
+    }
+
+    printf("Interesting marks count = %d", interesting_marks);
 
     return 0;
 }
---
+++ Term_Final_Practice/q16.c
@@ -0,0 +1,18 @@
+/*
+QB: 13 Page (1(a, b))
+*/
+#include <stdio.h>
+
+int main(){
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
